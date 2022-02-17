b845a4f6b3bb54e139caa7c37d3bb5ab8f73ff9b
RyanCShelley <94121225+RyanCShelley@users.noreply.github.com>
2022-02-17 13:07:09 -0500

    Update inlinks_topics.py

--- inlinks_topics.py
+++ inlinks_topics.py
@@ -23,7 +23,29 @@ st.subheader('Upload Your Crawl Data')
 
 uploaded_file = st.file_uploader("Choose a file")
 
-if uploaded_file is not None:
-	dataframe = pd.read_csv(uploaded_file)
+	if uploaded_file is not None:
+		dataframe = pd.read_csv(uploaded_file)
      
 st.write(dataframe)
+
+df = pd.read_csv(dataframe)
+
+st.subheader('Topic Annotations')
+
+st.caption('Below is the list of URLs with the associated topics.')
+
+concepts = []
+
+# Loop items in results
+for page in df['Address']:
+  response = natural_language_understanding.analyze(
+    url= page,
+    features=Features(concepts=ConceptsOptions(limit=1))).get_result()
+  if response is not None:
+    concepts.append(response)
+
+
+df_concepts_full = pd.json_normalize(concepts)
+df_concepts_full['concepts'] = pd.json_normalize(df_concepts_full['concepts'])
+df_concepts_full = pd.concat([df_concepts_full[['retrieved_url']], pd.json_normalize(df_concepts_full['concepts'])], axis=1)
+st.dataframe(df_concepts_full)

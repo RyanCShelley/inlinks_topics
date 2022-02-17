83c0ff6c604c2de02966fa0e4344b22e156e90af
RyanCShelley <94121225+RyanCShelley@users.noreply.github.com>
2022-02-17 15:49:15 -0500

    Update inlinks_topics.py

--- inlinks_topics.py
+++ inlinks_topics.py
@@ -25,6 +25,8 @@ uploaded_file = st.file_uploader("Choose a file")
 
 if uploaded_file is not None:
 		df = pd.read_csv(uploaded_file)
+else:
+    print('Please upload a file')
 
 st.subheader('Topic Annotations')
 
